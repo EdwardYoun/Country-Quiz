f955f8b8af42e511674920f3f4ac9e9f1b871f10
EdwardYoun <82770744+EdwardYoun@users.noreply.github.com>
2023-03-29 10:52:59 -0400

    countriesData class with Country

---
+++ app/src/main/java/edu/uga/cs/countryquiz/Country.java
@@ -0,0 +1,55 @@
+package edu.uga.cs.countryquiz;
+
+/**
+ * This class (a POJO) represents a single country, including the id, country name,
+ * and country's continent.
+ * The id is -1 if the object has not been persisted in the database yet, and
+ * the db table's primary key value, if it has been persisted.
+ */
+public class Country {
+    private long   id;
+    private String name;
+    private String continent;
+
+    public Country()
+    {
+        this.id = -1;
+        this.name = null;
+        this.continent = null;
+    }
+
+    public Country( String name, String continent) {
+        this.id = -1;  // the primary key id will be set by a setter method
+        this.name = name;
+        this.continent = continent;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id) { this.id = id; }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getContinent()
+    {
+        return continent;
+    }
+
+    public void setContinent(String continent) { this.continent = continent; }
+
+    public String toString()
+    {
+        return id + ": " + name + " " + continent;
+    }
+}
