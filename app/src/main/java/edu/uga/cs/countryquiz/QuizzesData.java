8068fc4791b35c379422e90a3609d7b7ca3b66e0
EdwardYoun <ey26416@uga.edu>
2023-03-30 23:44:33 -0400

    gradle update

---
+++ app/src/main/java/edu/uga/cs/countryquiz/QuizzesData.java
@@ -0,0 +1,17 @@
+package edu.uga.cs.countryquiz;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class is facilitates storing and restoring quizzes stored.
+ */
+public class QuizzesData {
+}
