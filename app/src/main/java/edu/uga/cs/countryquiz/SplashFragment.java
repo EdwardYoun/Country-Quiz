8dcab3346b893c64386b0e2416257be8a3eb1637
pennogilvie <wpo92700@uga.edu>
2023-04-03 18:59:20 -0400

    Added basic explaination on starting screen.

--- app/src/main/java/edu/uga/cs/countryquiz/SplashFragment.java
+++ app/src/main/java/edu/uga/cs/countryquiz/SplashFragment.java
@@ -1,11 +1,14 @@
 package edu.uga.cs.countryquiz;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -24,6 +27,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
@@ -32,13 +36,14 @@ import java.io.InputStreamReader;
  * Use the {@link SplashFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SplashFragment extends Fragment {
+public class SplashFragment extends Fragment{
 
     private static final String TAG = "SplashFragment";
 
     private CountriesData countriesData = null;
 
     private List<Country> countriesList;
+
 
     public SplashFragment() {
         // Required empty public constructor
@@ -59,16 +64,20 @@ public class SplashFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false);
+
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState ) {
         super.onViewCreated( view, savedInstanceState );
 
+
         Button quiz = getView().findViewById(R.id.button);
         Button result = getView().findViewById(R.id.button2);
 
         //setOnClickListener will be for other activities
+
 
         countriesList = new ArrayList<Country>();
 
