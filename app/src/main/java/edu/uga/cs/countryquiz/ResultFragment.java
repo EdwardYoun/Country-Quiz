e2bcb44b480d381787d9e990d0407d7885ca3d7b
EdwardYoun <ey26416@uga.edu>
2023-04-04 23:07:54 -0400

    working on result

--- app/src/main/java/edu/uga/cs/countryquiz/ResultFragment.java
+++ app/src/main/java/edu/uga/cs/countryquiz/ResultFragment.java
@@ -2,11 +2,17 @@ package edu.uga.cs.countryquiz;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +20,10 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ResultFragment extends Fragment {
+
+    private QuizzesData quizzesData = null;
+
+    private List<Quiz> quizzesList;
 
     public ResultFragment() {
         // Required empty public constructor
@@ -34,5 +44,35 @@ public class ResultFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_result, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState ) {
+        super.onViewCreated(view, savedInstanceState);
+
+        quizzesData = new QuizzesData(getActivity());
+        quizzesData.open();
+        quizzesList = quizzesData.retrieveAllQuizzes();
+
+        TableLayout tableLayout = getView().findViewById(R.id.table1);
+
+        // set up margins for each TextView in the table layout
+        android.widget.TableRow.LayoutParams layoutParams =
+                new TableRow.LayoutParams( TableRow.LayoutParams.WRAP_CONTENT,
+                        TableRow.LayoutParams.WRAP_CONTENT );
+        layoutParams.setMargins(20, 0, 20, 0);
+
+        for (int i = 0; i < quizzesList.size(); i++) {
+            TableRow tableRow = new TableRow( getContext() );
+            TextView textView1 = new TextView( getContext() );
+            TextView textView2 = new TextView( getContext() );
+
+            textView1.setText(quizzesList.get(i).getDate());
+            textView2.setText(quizzesList.get(i).getResult());
+            tableRow.addView( textView1, layoutParams );
+            tableRow.addView( textView2, layoutParams );
+            // add the next row to the table layout
+            tableLayout.addView( tableRow );
+        }
     }
 }
